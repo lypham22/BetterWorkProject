6f7d24f337e408523df9888517aa2a409ba45235
tamtv <tamtv93@gmail.com>
2015-12-18 09:30:22 +0700

    Add file UserDetailsDTO

---
+++ BW.Data.Contracts/BW.Data.Contract/DTOs/User/UserDetailsDTO.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+using System.Collections.Generic;
+namespace BW.Data.Contract.DTOs
+{
+    public class UserDetailsDTO
+    {
+        public UserDTO user { get; set; }
+        public List<RoleDTO> roles { get; set; }
+    }
+}
